2865d8e1237a0489039e3818888b8aadd4982513
Edward Junior <etdj@aluno.ifnmg.edu.br>
2022-06-03 02:06:14 -0300

    Onion Architecture

---
+++ 03-Onion Architecture Implementation/CompanyEmployees/Service/CompanyService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+internal sealed class CompanyService : ICompanyService
+{
+    private readonly ILoggerManager _logger;
+    private readonly IRepositoryManager _repository;
+
+    public CompanyService(IRepositoryManager repository, ILoggerManager logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+}
