ce1cda4d232d507f14be4b3027b7e0da8a1a443c
Edward Junior <etdj@aluno.ifnmg.edu.br>
2022-06-03 02:14:37 -0300

    --allow-empty-message

---
+++ 03-Onion Architecture Implementation/CompanyEmployees/Service/EmployeeService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+internal sealed class EmployeeService : IEmployeeService
+{
+    private readonly ILoggerManager _logger;
+    private readonly IRepositoryManager _repository;
+
+    public EmployeeService(IRepositoryManager repository, ILoggerManager logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+}
